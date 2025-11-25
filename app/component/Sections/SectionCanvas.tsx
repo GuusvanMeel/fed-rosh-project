216d0020c1d88a2e83f3d4ee7291a5b5cec3cce4
Sev3r <sjoerdever04@gmail.com>
2025-11-25 11:09:03 +0100

    waste

--- app/component/Sections/SectionCanvas.tsx
+++ app/component/Sections/SectionCanvas.tsx
@@ -466,11 +466,6 @@ function PanelSettingsForm({
                             onChange={handleFileUpload}
                         />
                     </label>
-                    {/* {panel.panelProps.content && (
-                        <span className="text-xs text-green-400 mt-2">
-                            ✓ File uploaded
-                        </span>
-                    )} */}
                 </div>
             )}
 
