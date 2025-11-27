0872c06ce182a2433e7d3e35d68bb78aef8a07aa
Maarten <92666704+Maarten0162@users.noreply.github.com>
2025-11-27 10:30:27 +0100

    component retrieved from older verson

---
+++ app/component/UploadWidget.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { CldUploadWidget } from 'next-cloudinary';
+
+
+
+
+export default function UploadWidget() {
+  return (
+    <CldUploadWidget uploadPreset="RoshApp"   onSuccess={(result) => {
+
+        const info = result.info;
+        if (info != null && typeof info !== "string") {
+        const url = info.secure_url; // now TS knows this exists
+        console.log("Uploaded image URL:", url);
+        //updatePanelContent(selectedPanel.i, url)
+
+        }
+
+        }}>
+        {
+            ({open}) => {
+                return <button className="w-full bg-neutral-800 p-2 rounded" onClick={() => open()} > Upload an Image</button>
+            }
+        }
+
+    </CldUploadWidget>
+  )
+}
