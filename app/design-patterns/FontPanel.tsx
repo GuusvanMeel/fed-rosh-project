64f61bdc5cc0656f2586a1ef36181f0d36a8d5a5
Jeffrey <jeffreyvantillo@gmail.com>
2025-12-02 10:44:38 +0100

    Removed button to avoid double functionality

--- app/design-patterns/FontPanel.tsx
+++ app/design-patterns/FontPanel.tsx
@@ -13,38 +13,28 @@ export default function FontPicker() {
   return (
     <Box>
       <NativeSelectRoot className="bg-gray-900 radius-xl"
-  onChange={(e) => {
-    setFont((e.target as HTMLSelectElement).value);
-    console.log("Font changed to:", (e.target as HTMLSelectElement).value);
-  }}
->
-  <NativeSelectField
-  value={font}
-  onChange={(e) => {
-    setFont(e.target.value);
-    console.log("Font changed:", e.target.value);
-  }}
->
-  <option value="">Select font</option>
-
-  {fontList.map((f) => (
-    <option key={f} value={f} style={{ fontFamily: f }}>
-      {f}
-    </option>
-  ))}
-</NativeSelectField>
-</NativeSelectRoot>
-
-      <Button
-        onClick={() => {
-          console.log("Apply font button clicked");
-          applyFontToAllPanels?.();
+        onChange={(e) => {
+            setFont((e.target as HTMLSelectElement).value);
+            console.log("Font changed to:", (e.target as HTMLSelectElement).value);
         }}
-        width="100%"
-        mt={2}
-      >
-        Apply font to all panels
-      </Button>
+        >
+            <NativeSelectField
+            value={font}
+            onChange={(e) => {
+                setFont(e.target.value);
+                console.log("Font changed:", e.target.value);
+                applyFontToAllPanels?.();
+            }}
+            >
+            <option value="">Select font</option>
+
+            {fontList.map((f) => (
+                <option key={f} value={f} style={{ fontFamily: f }}>
+                {f}
+                </option>
+            ))}
+            </NativeSelectField>
+        </NativeSelectRoot>
     </Box>
   );
 }
